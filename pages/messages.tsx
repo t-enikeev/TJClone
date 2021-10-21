155bc29a41cbbbaecef8b40bfa3710b1faf2c4b3
redchrysler <redchrysler@proton-mail.com>
2021-10-21 17:51:39 +0300

    Добавил роутинг и доверстал элементы авторизации

---
+++ pages/messages.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { MainLayout } from "../layouts/MainLayout";
+
+export const Messages = () => {
+  return (
+    <MainLayout>
+      <h1>Сообщения</h1>
+    </MainLayout>
+  );
+};
+
+export default Messages;
